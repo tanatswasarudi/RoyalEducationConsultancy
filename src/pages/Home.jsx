0d26530920ae62c281d3f21d97f990b89c9eb1b3
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-08-05 00:49:50 +0530

    buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
         <div style={{backgroundImage: `url(${slides[currentIndex]})`}} className="w-full h-[400px] md:h-[780px]  rounded-2xl bg-center bg-cover duration-500"></div>
      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/25 text-white cursor-pointer">
       <BsArrowLeftShort onClick={prevSlide} size={30}/></div>
-     <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/25 text-white cursor-pointer">
+     <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/25 text-white cursor-pointer">
       <BsArrowRightShort onClick={nextSlide} size={30}/></div>
       <div className="flex top-3 justify-center">
       {slides.map((slide,slideIndex)=>(
