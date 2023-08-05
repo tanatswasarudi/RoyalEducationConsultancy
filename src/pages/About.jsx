13643bd1973b9f34efa581f5a6990e1aad3d65f3
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-08-05 18:56:34 +0530

    done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ import img3 from '../Assets/student3.jpg';
 import img4 from '../Assets/student4.jpg';
 import img5 from '../Assets/student5.jpg';
 import img6 from '../Assets/student6.jpg';
-
+import logo from '../Assets/Logo.jpg'
 const About = () => {
   const images = [img1, img2, img3, img4, img5, img6];
   const [currentIndigo, setCurrentIndigo] = useState(0)
@@ -26,8 +26,8 @@ const goToNext = () => {
 }
   return (
     <div>
-      <div className="max-w-[1400px] h-[780px] py-16 px-4 w-full m-auto relative group">
-        <div style={{ backgroundImage: `url(${images[currentIndigo]})` }} className="w-full h-full object-cover rounded-2xl duration-500"></div>
+      <div className="md:max-w-[1400px] max-w-[450px] md:h-[780px] h-[300px]  py-16 px-4 w-full m-auto relative group">
+        <div style={{ backgroundImage: `url(${images[currentIndigo]})` }} className="w-full h-full rounded-2xl duration-500"></div>
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/25 text-white cursor-pointer">
           <BsArrowLeftShort onClick={goToPrevious} size={20} />
         </div>
@@ -38,6 +38,22 @@ const goToNext = () => {
           {images.map((image, imageIndex) => (
             <div key={imageIndex} onClick={() => goToImage(imageIndex)} className=""><RxDotFilled className='text-2xl' /></div>
           ))}
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-6 mt-10 ">
+        <div className="w-[500px] h-[400px] ">
+          <img src={logo} alt="" className='w-full h-full' />
+        </div>
+        <div className="flex flex-col items-center px-3">
+          <span className='text-2xl font-serif text-blue-950'>WHO ARE WE</span>
+          <p className='text-xl text-red-500'>AN UPCOMING RELIABLE AND AUTHENTIC EDUCATION CONSULTANCY</p>
+          <p className='mt-4'>Our aim is to provide various educational opportunities to all our clients at fingertips points at an affordable price</p>
+           <p className=''>Royal Education is built on love and passion to provide affordable educational opportunities for all.</p>
+           <p>Our goal is to reach out to as many people as possible and deliverthese opportunities at an affordable price</p>
+           <p>We provide full assistance throughout the admission process: counselling students in their home countries, liaising with the Indian embassy to expedite student visas, providing online money transfer platforms that make money transfer an effortless process</p>
+           <p>Royal Education Consultancy is committed to fulfil the dream of aspiring students to get quality education by providing them intriguing scholarship programs in India’s top universities. Our students take advantage of the many distinctive courses that our colleges provide for them which helps them to choose a career of their dreams.</p>
+           <p className='text-gray-500 '></p>
+           
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,8 +27,8 @@ const Home = () => {
 
   return (
     <div className="">
-      <div className=" md:h-[500px] h-[300px] w-full m-auto relative group">
-        <div style={{ backgroundImage: `url(${slides[currentIndex]})` }} className=" w-full h-full object-cover rounded-2xl duration-500"></div>
+      <div className="md:max-w-[1400px] max-w-[450px] md:h-[780px] h-[300px] w-full m-auto relative group">
+        <div style={{ backgroundImage: `url(${slides[currentIndex]})` }} className=" w-full h-full rounded-2xl duration-500"></div>
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/25 text-white cursor-pointer">
           <BsArrowLeftShort onClick={prevSlide} size={30} />
         </div>
@@ -41,8 +41,46 @@ const Home = () => {
           ))}
         </div>
       </div>
+      <div className="mt-10 grid md:grid-cols-2 grid-cols-1 gap-6">
+        <div className="">
+          <p className='text-blue-950 font-serif'>WHY CHOOSE US</p>
+          <p className='mt-6'>Royal Education Consultancy doesn't just specialize on scholarships to India, we are linked with a large number of universities in Europe in countries like Canada, Poland, Italy and Germany etc. We also provide support at every stage of the process, from accepting enrolment to the hassle-free arrival of students. </p>
+          <p>We are also linked with other countries such as Ghana, South Africa, Indonesia, Thailand and China.This is to ensure that our students get the best education at a lower cost as well as the ability to choose their dream destination</p>
+          <p>After our students have arrived at their destinations, we also provide them with accomodation free of charge for the first month to allow them to settle</p>
+            <p>Our Services also include:</p>
+            <div className='grid grid-cols-2 gap-3'>
+            <span className='flex flex-col items-center'>
+            <span className='flex items-center font-semibold'><RxDotFilled className='text-red-500'/>Top Universities</span>
+            <span className='flex items-center font-semibold mt-4'><RxDotFilled className='text-red-500'/>Free Flight</span>
+            </span>
 
-      
+            <span className='flex flex-col items-center'>
+            <span className='flex items-center font-semibold'><RxDotFilled className='text-red-500'/>Accomodation</span>
+            <span className='flex items-center font-semibold mt-4'><RxDotFilled className='text-red-500'/>Partial Scholarship</span>
+            </span>
+            </div>
+            <div className="mt-5 grid-cols-3 grid gap-3">
+              <div className="flex flex-col">
+                <span className='flex items-center'><span className='text-2xl text-blue-950'>10K</span><span className='text-red-500 text-xl'>+</span></span>
+                <span className='text-gray-500'>International Students</span>
+              </div>
+              <div className="flex flex-col">
+                <span className='flex items-center'><span className='text-2xl text-blue-950'>100</span><span className='text-red-500 text-xl'>+</span></span>
+                <span className='text-gray-500'>Top Universities</span>
+              </div>
+              <div className="flex flex-col">
+                <span className='flex items-center'><span className='text-2xl text-blue-950'>50</span><span className='text-red-500 text-xl'>+</span></span>
+                <span className='text-gray-500'>Companies</span>
+              </div>
+            </div>
+            <div className="mt-5">
+              <button className='py-2 px-3 shadow flex items-center bg-red-600'>Know more <BsArrowRight className='text-xl text-white'/></button>
+            </div>
+        </div>
+        <div className="">
+          
+        </div>
+      </div>
     </div>
   )
 }
