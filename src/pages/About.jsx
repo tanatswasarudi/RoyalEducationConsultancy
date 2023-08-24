33f857da689c2fafc1f2c4515795dd2f7a621926
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-08-24 13:32:28 +0530

    docs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,11 +28,11 @@ const goToNext = () => {
 }
   return (
     <div>
-     <div className="w-full relative group">
-  <div
-    style={{ backgroundImage: `url(${images[currentIndigo]})` }}
-    className="w-full h-[300px] md:h-[400px] bg-cover bg-center rounded-2xl duration-500"
-  ></div>
+     <div className="w-full md:h-[500px] h-[300px] relative group">
+        <div
+         style={{ backgroundImage: `url(${images[currentIndigo]})` }}
+         className="w-full h-auto bg-cover bg-center rounded-2xl duration-500" width="600" height="300">
+           </div>
   <div className="hidden group-hover:block absolute top-1/2 transform -translate-y-1/2 left-5 text-2xl rounded-full p-2 bg-black/25 text-white cursor-pointer">
     <BsArrowLeftShort onClick={goToPrevious} size={20} />
   </div>
